d9478836c83633266b8db03e29700bef74be78f9
Ian <iandos@gmail.com>
2020-05-23 18:33:56 -0300

    Dispatch an action when permission is granted.

--- src/scenes/requestLocation.js
+++ src/scenes/requestLocation.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Image} from 'react-native';
 import styled from 'styled-components';
+import RNLocation from 'react-native-location';
+import {useDispatch} from 'react-redux';
+import {PINK_COLOR, BLUE_COLOR} from '../utils/colors';
+import {setPermissionStatus} from '../redux/actions';
 
 const MooView = styled.SafeAreaView`
   flex: 1;
@@ -10,7 +14,7 @@ const MooView = styled.SafeAreaView`
 `;
 
 const MooBigText = styled.Text`
-  color: #252436;
+  color: ${BLUE_COLOR};
   font-size: 36px;
   font-family: 'Merriweather-Bold';
   padding: 20px;
@@ -24,17 +28,30 @@ const MooText = styled.Text`
 `;
 
 const MooButton = styled.TouchableOpacity`
-  background-color: #ff0066;
+  background-color: ${PINK_COLOR};
   padding: 15px;
   border-radius: 10px;
 `;
 
 const RequestLocation = (props) => {
+  const dispatch = useDispatch();
+
+  const onPressRequestLocation = () => {
+    RNLocation.requestPermission({
+      ios: 'whenInUse',
+      android: {
+        detail: 'coarse',
+      },
+    }).then((granted) => {
+      dispatch(setPermissionStatus(granted));
+    });
+  };
+
   return (
     <MooView>
       <Image source={require('../assets/images/undraw_my_location.png')} />
       <MooBigText>Oi, precisamos de sua localização</MooBigText>
-      <MooButton>
+      <MooButton onPress={onPressRequestLocation}>
         <MooText>Autorizar acesso</MooText>
       </MooButton>
     </MooView>
