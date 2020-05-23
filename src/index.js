129e5243e9764f305f0f74f9a24da29c276620b1
Ian <iandos@gmail.com>
2020-05-23 18:37:23 -0300

    Sets up redux libraries to work together.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
-import RequestLocation from './scenes/requestLocation';
+import {Provider} from 'react-redux';
+import rootReducer from './redux/reducers';
+import {createStore, applyMiddleware} from 'redux';
+import Home from './scenes/home';
+import {ReduxNetworkProvider} from 'react-native-offline';
+import {createNetworkMiddleware} from 'react-native-offline';
+import thunkMiddleware from 'redux-thunk';
+import logger from 'redux-logger';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
+import {PersistGate} from 'redux-persist/integration/react';
+import Loading from './scenes/loading';
 
-const App = () => <RequestLocation />;
+const networkMiddleware = createNetworkMiddleware();
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  blacklist: ['network', 'permission'],
+  timeout: 0,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(
+  persistedReducer,
+  applyMiddleware(networkMiddleware, thunkMiddleware, logger),
+);
+const persistor = persistStore(store);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ReduxNetworkProvider pingInterval={20000}>
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <Home />
+        </PersistGate>
+      </ReduxNetworkProvider>
+    </Provider>
+  );
+};
 
 export default App;
